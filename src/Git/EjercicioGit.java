de5628588ae29b2191120a7941178025c94ee978
AlejandroRC <alejandrorebola91@gmail.com>
2022-04-13 12:09:36 +0200

    tercer commit

--- src/Git/EjercicioGit.java
+++ src/Git/EjercicioGit.java
@@ -4,8 +4,17 @@ public class EjercicioGit {
 
 	public static void main(String[] args) {
 
+		/**
+		 * @author Alejandro Rebola Casquero
+		 *
+		 */
+
+		// Añadiendo el primer commit en git
 
 		System.out.println("Hola mundo!");
+
+		// Añadiendo el segundo commit en git
+
 		System.out.println("Hola Alejandro Rebola Casquero!");
 
 	}
